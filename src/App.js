220d91b0d799b6836a6b6fd5904216a75cc1b0b0
radin-reth <radin.reth@gmail.com>
2022-01-11 23:34:05 +0700

    Add basic footer

--- src/App.js
+++ src/App.js
@@ -23,8 +23,20 @@ const App = () => {
             </Routes>
           </div>
         </Layout>
+
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: "white", textAlign: "center" }}>
+            Cryptoverse <br />
+            All right reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   )
 }
