f4cedbb4c6c55537e309fa844444a8edc454ce26
radin-reth <radin.reth@gmail.com>
2022-01-12 17:22:28 +0700

    Render crypto currencies list

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import millify from 'millify'
+import { Link } from 'react-router-dom'
+import { Card, Col, Row, Input } from 'antd'
 
-const Cryptocurrencies = () => {
+import { useGetCryptosQuery } from '../services/cryptoApi'
+
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100
+
+  const { data: cryptoList, isFetching } = useGetCryptosQuery(count)
+  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins)
+
+  if(isFetching) return 'loading...'
+
   return (
-    <div>
-      Cryptocurrencies
-    </div>
+    <>
+      <Row gutter={[12, 12]} className='crypto-card-container'>
+        {
+          cryptos?.map((crypto) => (
+            <Col xs={24} sm={12} md={6} className="crypto-card" key={crypto.uuid}>
+              <Link to={`/crypto/${crypto.uuid}`}>
+                <Card
+                  title={`${crypto.rank}. ${crypto.name}`}
+                  extra={<img src={crypto.iconUrl} className="crypto-image" />}
+                  hoverable
+                >
+                  <p>Price: {millify(crypto.price)}</p>
+                  <p>Market Cap: {millify(crypto.marketCap)}</p>
+                  <p>Daily Change: {millify(crypto.change)}%</p>
+                </Card>
+              </Link>
+            </Col>
+          ))
+        }
+      </Row>
+    </>
   )
 }
 
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom'
 
 import { useGetCryptosQuery } from '../services/cryptoApi'
 
+import { Cryptocurrencies, News } from '../components'
+
 const { Title } = Typography
 
 const Homepage = () => {
 
-  const {data, isFetching} = useGetCryptosQuery()
+  const {data, isFetching} = useGetCryptosQuery(10)
   const globalStats = data?.data?.stats;
 
   if(isFetching) return "Loading..."
@@ -34,6 +36,18 @@ const Homepage = () => {
           <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
         </Col>
       </Row>
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 cryptocurrencies</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">See more</Link></Title>
+      </div>
+      <Cryptocurrencies simplified />
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3} className="show-more"><Link to="/news">See more</Link></Title>
+      </div>
+      <News simplified />
     </>
   )
 }
